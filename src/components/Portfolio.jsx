f80a5fa2cd9596d83fc4c48413a8c5bedef380a0
mjohanbintangp <m.johanbintang.pratama@gmail.com>
2025-01-17 07:19:46 +0700

    text in portfolio card

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -15,7 +15,7 @@ const Portfolio = () => {
                 <h1 className="bg-transparent text-[#D4D4D4] text-[25px] font-semibold">Cooking</h1>
               </div>
             </div>
-            <p className="text-white text-xs mt-4 lg:text-left text-center">Calm our mind . Mobile App</p>
+            <p className="text-white text-xs mt-4 lg:text-left text-center">Mobile App . Web App</p>
             <h3 className="text-white text-xl font-semibold lg:text-left text-center">Mindease</h3>
           </div>
 
@@ -23,18 +23,18 @@ const Portfolio = () => {
           <div>
             <div className="bg-[#454545] w-[300px] h-[300px] p-5 rounded-[50px] flex justify-center items-center text-center">
               <div className="bg-transparent">
-                <h1 className="bg-transparent text-[#D4D4D4] text-[25px] font-semibold">Cooking</h1>
+                <h1 className="bg-transparent text-[#D4D4D4] text-[25px] font-semibold">ComingSoon</h1>
               </div>
             </div>
-            <p className="text-white text-xs mt-4 lg:text-left text-center">Calm our mind . Web App</p>
-            <h3 className="text-white text-xl font-semibold lg:text-left text-center">Mindease</h3>
+            <p className="text-white text-xs mt-4 lg:text-left text-center">Coming . Soon</p>
+            <h3 className="text-white text-xl font-semibold lg:text-left text-center">ComingSoon</h3>
           </div>
 
           {/* Card 3 */}
           <div>
             <div className="bg-[#454545] w-[300px] h-[300px] p-5 rounded-[50px] flex justify-center items-center text-center">
               <div className="bg-transparent">
-                <h1 className="bg-transparent text-[#D4D4D4] text-[25px] font-semibold">Cooking</h1>
+                <h1 className="bg-transparent text-[#D4D4D4] text-[25px] font-semibold">CoomingSoon</h1>
               </div>
             </div>
             <p className="text-white text-xs mt-4 lg:text-left text-center">Coming . Soon</p>
@@ -45,7 +45,7 @@ const Portfolio = () => {
           <div>
             <div className="bg-[#454545] w-[300px] h-[300px] p-5 rounded-[50px] flex justify-center items-center text-center">
               <div className="bg-transparent">
-                <h1 className="bg-transparent text-[#D4D4D4] text-[25px] font-semibold">Cooking</h1>
+                <h1 className="bg-transparent text-[#D4D4D4] text-[25px] font-semibold">ComingSoon</h1>
               </div>
             </div>
             <p className="text-white text-xs mt-4 lg:text-left text-center">Coming . Soon</p>
@@ -56,7 +56,7 @@ const Portfolio = () => {
           <div>
             <div className="bg-[#454545] w-[300px] h-[300px] p-5 rounded-[50px] flex justify-center items-center text-center">
               <div className="bg-transparent">
-                <h1 className="bg-transparent text-[#D4D4D4] text-[25px] font-semibold">Cooking</h1>
+                <h1 className="bg-transparent text-[#D4D4D4] text-[25px] font-semibold">ComingSoon</h1>
               </div>
             </div>
             <p className="text-white text-xs mt-4 lg:text-left text-center">Coming . Soon</p>
@@ -67,7 +67,7 @@ const Portfolio = () => {
           <div>
             <div className="bg-[#454545] w-[300px] h-[300px] p-5 rounded-[50px] flex justify-center items-center text-center">
               <div className="bg-transparent">
-                <h1 className="bg-transparent text-[#D4D4D4] text-[25px] font-semibold">Cooking</h1>
+                <h1 className="bg-transparent text-[#D4D4D4] text-[25px] font-semibold">CoomingSoon</h1>
               </div>
             </div>
             <p className="text-white text-xs mt-4 lg:text-left text-center">Coming . Soon</p>
